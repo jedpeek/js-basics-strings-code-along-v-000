683ae24b91cd1ed90ad11269ef2f6097c64396b2
Jed Peek <jedpeek@yahoo.com>
2018-08-31 19:24:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Jed";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + '!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.upcase + '!';
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
