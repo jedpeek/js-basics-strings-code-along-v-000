9124dcaf0b0752d40ca44ddced8d6dd27a03eb79
Jed Peek <jedpeek@yahoo.com>
2018-08-31 19:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
-let currentUser = "Me"
-const welcomeMessage = 'Welcome to Flatbook, ';
+let currentUser = "Me";
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
